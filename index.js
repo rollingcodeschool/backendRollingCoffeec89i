44911b8cb78edca698f033a86e890247eca48ca6
Emilse Arias <emilsearias@gmail.com>
2024-05-15 19:58:21 -0300

    podemos escuchar un puerto en nuestro backend

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hola mundo nuevo')
+import express from 'express';
+
+//1- configurar un puerto
+const app= express();
+//crear una variable
+app.set('port', process.env.PORT || 4000 );
+app.listen(app.get('port') ,()=>{
+    console.info('Estoy escuchando el puerto '+app.get('port') )
+})
+
+//2- configurar middlewares
+
+//3- crear las rutas
