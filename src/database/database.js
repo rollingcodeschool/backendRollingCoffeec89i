6c48f064c43c536d7cf258e347ea08cc144f08f2
Emilse Arias <emilsearias@gmail.com>
2024-05-20 19:34:36 -0300

    agregamos la conexion a la BD en el archivo database.js

--- src/database/database.js
+++ src/database/database.js
@@ -1,3 +1,11 @@
 import mongoose from "mongoose";
 
-console.log(process.env.MONGODB_URI)
+const mongodb= process.env.MONGODB_URI;
+
+mongoose.connect(mongodb)
+
+const conexion = mongoose.connection;
+
+conexion.once('open', ()=>{
+    console.info('BD conectada')
+})
