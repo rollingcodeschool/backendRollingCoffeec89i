5769c17296f33fc0e91eba9d29248267f5c550fa
Emilse Arias <emilsearias@gmail.com>
2024-05-22 21:16:36 -0300

    agregamos la logica de la funcion get para obtener un producto y controlamos dos tipos de errores

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,27 +1,48 @@
 import Producto from "../database/models/producto.js";
 
-export const listarProductos = async(req, res) => {
-  try{
+export const listarProductos = async (req, res) => {
+  try {
     //pedirle a la BD los productos
     const productos = await Producto.find();
     // enviar la respuesta con los productos
     res.status(200).json(productos);
-   
-  }catch(error){
-    console.error(error)
-    res.status(500).json({mensaje: 'Ocurrio un error al listar los productos'})
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ mensaje: "Ocurrio un error al listar los productos" });
   }
 };
 
-export const crearProducto = async(req, res) => {
+export const obtenerProducto = async (req, res) => {
+  try {
+    //extraer el id o parametro del request
+    console.log(req.params.id);
+    //pedir a la bd buscar ese producto
+    const productoBuscado = await Producto.findById(req.params.id);
+    //quiero saber si productoBuscado es null
+    if(!productoBuscado){
+      return res.status(400).json({mensaje: `El producto con id: ${req.params.id} no existe`});
+    }
+    //responder al front con el producto buscado
+    res.status(200).json(productoBuscado);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ mensaje: "Ocurrio un error al obtener el producto" });
+  }
+};
+
+export const crearProducto = async (req, res) => {
   try {
     //validar los datos del producto del ----
     //le vamos a pedir a la bd crear el producto
     console.log(req.body);
-    const productoNuevo = new Producto(req.body)
+    const productoNuevo = new Producto(req.body);
     await productoNuevo.save();
     //enviar la respuesta de lo sucedido
-    res.status(201).json({mensaje: 'El producto fue creado correctamente'})
+    res.status(201).json({ mensaje: "El producto fue creado correctamente" });
   } catch (error) {
     console.error(error);
     res.status(500).json({
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { crearProducto, listarProductos } from "../controllers/productos.controllers.js";
+import { crearProducto, listarProductos, obtenerProducto } from "../controllers/productos.controllers.js";
 const router = Router();
 
 //crear las rutas
 router.route('/prueba').get(listarProductos);
-router.route('/productos').post(crearProducto).get(listarProductos)
+router.route('/productos').post(crearProducto).get(listarProductos);
+router.route('/productos/:id').get(obtenerProducto)
 export default router
